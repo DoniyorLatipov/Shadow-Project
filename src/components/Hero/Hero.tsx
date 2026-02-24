55c6535c7b4fb1d9037a74cfb306b180aedb017f
DoniyorLatipov <doniyor.main.contact@gmail.com>
2026-02-24 11:45:08 +0500

    fix: correct typo in text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,8 +8,8 @@ export default function Hero() {
       <Container className={classes.heroContainer}>
         <h1 className={classes.title}>Натяжные потолки</h1>
         <p className={classes.subtitle}>
-          Работаем с готовыми потолками, сложными случаями и профилем EuroKRAAB по Санкт-Петербург и
-          Ленинградской области
+          Работаем с готовыми потолками, сложными случаями и профилем EuroKRAAB по Санкт-Петербургу
+          и Ленинградской области
         </p>
         <Button className={classes.callButton}>
           <a href="tel:+79643749631">Позвонить</a>
